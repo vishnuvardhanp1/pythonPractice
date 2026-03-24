2e24d881716e7bf9a6bc898dfc1cf7b7738f2e91
vishnuvardhanp1 <vishnu.vis2007@gmail.com>
2026-03-24 11:02:55 +0530

    24 mar python variables

---
+++ basics/Program5_Varaibles.py
@@ -0,0 +1,73 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+# illegal variable names
+
+"""
+ 2myvar = "John"
+ my-var = "John"
+ my var = "John"
+
+"""
+
+
+#camelcase
+myVariableName = "John";
+#pascalcase
+MyVariableName = "John";
+#snakecase
+my_variable_name = "John";
+
+print(myVariableName);
+print(MyVariableName);
+print(my_variable_name);
+
+print(myvar);
+print(my_var);
+print(_my_var);
+print(myVar);
+print(MYVAR);
+print(myvar2);
+
+#Assign values to variables
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Assigning single value to multiple variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# output variables 
+x = "Python is awesome"
+print(x)
+
+# Printing multiple output variables
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# using + operator for output variables
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# mathematical operations
+x = 5
+y = 10
+print(x + y)
+
+# combining number and string
+x=5
+y="String"
+print(x,y)
+
